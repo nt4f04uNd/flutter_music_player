c80429d73247b30cbd0367cdbdd8a7636dfa3e2b
Abestanis <abestanis.gc@gmail.com>
2024-10-21 01:25:17 +0200

    Add app_widget to logic.dart

--- lib/logic/player/content.dart
+++ lib/logic/player/content.dart
@@ -5,7 +5,6 @@ import 'package:enum_to_string/enum_to_string.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 // import 'package:quick_actions/quick_actions.dart';
-import 'package:sweyer/logic/app_widget.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:sweyer/sweyer.dart';
 import 'package:sweyer_plugin/sweyer_plugin.dart';
